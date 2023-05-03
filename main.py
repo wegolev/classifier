ba1e09764e3ad56edae8ecc7e0077fe9857d204c
wegolev_work <a.wegolev@gmail.com>
2023-05-03 12:15:22 +0300

    import records

--- main.py
+++ main.py
@@ -4,4 +4,10 @@ path = 'data/lenta-ru-news.csv.gz'
 records = load_lenta(path)
 print(next(records))
 
+number_of_records = 1
 
+for record in records:
+    print(record.url, record.title, record.text, record.topic, record.tags, sep='\n')
+    if number_of_records == 1:
+        break
+        
