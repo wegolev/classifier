4d71878efc4fcdc576fc9aaacdb867c1d18c2739
wegolev_work <a.wegolev@gmail.com>
2023-05-04 14:49:58 +0300

    Read ONLY first N-lines

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from dataframe import df
+from corus import load_lenta
+from itertools import islice
 
-print(df.shape)
+path = 'data/lenta-ru-news.csv.gz'
+records = load_lenta(path)
+# Считать первые N строк:
+data = (next(records) for x in range(100))
+# data = list(islice(records, 0, 100))
+# оба варианта прочитают ТОЛЬКО N первых строк файла и остановятся на этом. Т.е. файл не будет читаться полностью в
+# отличие от варианта с file.readlines(). Только первый вариант позволяеть сохранить исходный <class 'generator'>
+print(type(data),  sep='\n')
+# print(df.head(5))
+
+
