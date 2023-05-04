21f703e28d85d071826b0ebdcabf959280287ef2
wegolev_work <a.wegolev@gmail.com>
2023-05-04 13:34:23 +0300

    Data to dataframe

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-from corus import load_lenta
+from dataframe import df
 
-path = 'data/lenta-ru-news.csv.gz'
-records = load_lenta(path)
-print(next(records))
-
-number_of_records = 1
-
-for record in records:
-    print(record.url, record.title, record.text, record.topic, record.tags, sep='\n')
-    if number_of_records == 1:
-        break
-        
+print(df.shape)
