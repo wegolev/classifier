1d1220187db89c27d9121af5f04cb9f33bfcc702
wegolev_work <a.wegolev@gmail.com>
2023-05-05 13:27:41 +0300

    work-pc off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas
 import re
+from nltk.corpus import stopwords
 from corus import load_lenta
 from itertools import islice
 
@@ -11,11 +11,12 @@ data = (next(records) for x in range(5))
 # оба варианта прочитают ТОЛЬКО N первых строк файла и остановятся на этом. Т.е. файл не будет читаться полностью в
 # отличие от варианта с file.readlines(). Только первый вариант позволяеть сохранить исходный <class 'generator'>
 
-# df = pandas.DataFrame(columns=['topic', 'title'])
-data_list = []
+data_list_lower_punctuation = []
 for data in data:
     # df.loc[len(df.index)] = [data.topic.lower(), data.title]
-    data_list.append([re.sub(r'[^\w\s]', '', data.topic.lower()), re.sub(r'[^\w\s]', '', data.title.lower())])
+    data_list_lower_punctuation.append([re.sub(r'[^\w\s]', '', data.topic.lower()),
+                                        [re.sub(r'[^\w\s]', '', data.title.lower())]])
 
-# print(df.dtypes, df.shape, df.head(5), sep='\n'
-print(len(data_list), data_list, sep='\n')
+    print(data_list_lower_punctuation[-1], sep='\n')
+
+print(len(data_list_lower_punctuation))
