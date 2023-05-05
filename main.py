9e5413fee3ba6a3931355c87ed2411fa27c58435
wegolev_p500ca <a.wegolev@gmail.com>
2023-05-05 17:29:22 +0300

    p500ca off day

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import re
+# import nltk
 from nltk.corpus import stopwords
 from corus import load_lenta
 from itertools import islice
 
+# nltk.download('stopwords')
+
 path = 'data/lenta-ru-news.csv.gz'
 records = load_lenta(path)
 # Считать первые N строк:
-data = (next(records) for x in range(5))
+data_iterator = (next(records) for x in range(5))
 # data = list(islice(records, 0, 100))
 # оба варианта прочитают ТОЛЬКО N первых строк файла и остановятся на этом. Т.е. файл не будет читаться полностью в
 # отличие от варианта с file.readlines(). Только первый вариант позволяеть сохранить исходный <class 'generator'>
 
 data_list_lower_punctuation = []
-for data in data:
-    # df.loc[len(df.index)] = [data.topic.lower(), data.title]
+data_list_lower_punctuation_stop_word = []
+for data in data_iterator:
     data_list_lower_punctuation.append([re.sub(r'[^\w\s]', '', data.topic.lower()),
-                                        [re.sub(r'[^\w\s]', '', data.title.lower())]])
+                                        (re.sub(r'[^\w\s]', '', data.title.lower())).split()])
 
-    print(data_list_lower_punctuation[-1], sep='\n')
+# for data_pair in data_list_lower_punctuation:
+#     for data in data_pair:
+#         if print(type(data))
 
-print(len(data_list_lower_punctuation))
+
+# print(stopwords.words('russian'))
+print(len(data_list_lower_punctuation), data_list_lower_punctuation[:], sep='\n')
+
