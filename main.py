b5575b14f23b55f7ba3f87bd014d896180513daf
wegolev_p500ca <a.wegolev@gmail.com>
2023-05-06 06:29:54 +0300

    before stem

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
 import re
-# import nltk
 from nltk.corpus import stopwords
 from corus import load_lenta
-from itertools import islice
 
+# from itertools import islice
+# import nltk
 # nltk.download('stopwords')
 
 path = 'data/lenta-ru-news.csv.gz'
 records = load_lenta(path)
 # Считать первые N строк:
-data_iterator = (next(records) for x in range(5))
+data_iterator = (next(records) for x in range(5)) # Данный вариант позволяеть сохранить исходный <class 'generator'>
 # data = list(islice(records, 0, 100))
-# оба варианта прочитают ТОЛЬКО N первых строк файла и остановятся на этом. Т.е. файл не будет читаться полностью в
-# отличие от варианта с file.readlines(). Только первый вариант позволяеть сохранить исходный <class 'generator'>
+# Файл не будет читаться полностью в отличие от варианта с file.readlines().
 
-data_list_lower_punctuation = []
-data_list_lower_punctuation_stop_word = []
+# 2. Обработка данных.:
+data_clear_list = []
 for data in data_iterator:
-    data_list_lower_punctuation.append([re.sub(r'[^\w\s]', '', data.topic.lower()),
-                                        (re.sub(r'[^\w\s]', '', data.title.lower())).split()])
+    data_topic_lower = data.topic.lower()
+    data_title_lower = data.title.lower()
+    data_topic_punctuation = re.sub(r'[^\w\s]', '', data_topic_lower) # Еще вариант составить список своих стоп слов
+    data_title_punctuation = re.sub(r'[^\w\s]', '', data_title_lower) # и проводить сравнение с ним
+    data_title_stop_word = [word for word in data_title_punctuation.split() if word not in stopwords.words('russian')]
 
-# for data_pair in data_list_lower_punctuation:
-#     for data in data_pair:
-#         if print(type(data))
+    data_clear_list.append([data_topic_punctuation, data_title_stop_word])
 
-
-# print(stopwords.words('russian'))
-print(len(data_list_lower_punctuation), data_list_lower_punctuation[:], sep='\n')
+print(f"len data list: {len(data_clear_list)}", *data_clear_list[:5], sep='\n') # печать списка в столбик. Еще вариант print('\n'.join(my_list))
 
