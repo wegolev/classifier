955df301ca9310c6f3e6ee4855b7d7d6159aaa3b
wegolev_work <a.wegolev@gmail.com>
2023-05-06 10:55:10 +0300

    train test split before vectorization

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import re
 from nltk.corpus import stopwords
 from corus import load_lenta
 from nltk.stem import SnowballStemmer
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
 
 # from itertools import islice
 # import nltk
@@ -16,6 +18,8 @@ data_iterator = (next(records) for x in range(5)) # Данный вариант 
 
 # 2. Обработка данных.:
 data_clear_list = []
+data_topic_clear_list = []
+data_title_clear_list = []
 for data in data_iterator:
     data_topic_lower = data.topic.lower()
     data_title_lower = data.title.lower()
@@ -26,7 +30,28 @@ for data in data_iterator:
     snowball = SnowballStemmer(language='russian')
     data_title_stem = list(map(snowball.stem, data_title_stop_words))
     
-    data_clear_list.append([data_topic_punctuation, data_title_stem])
 
-print(f"len data list: {len(data_clear_list)}", *data_clear_list[:5], sep='\n') # печать списка в столбик. Еще вариант print('\n'.join(my_list))
+    data_clear_list.append([data_topic_punctuation, data_title_stop_words])
+    data_topic_clear_list.append(data_topic_punctuation)
+    data_title_clear_list.append(data_title_stop_words)
+
+
+print(f'len data_clear list: {len(data_clear_list)}', *data_clear_list[:5], sep='\n')
+# print(f'len data_topic list: {len(data_topic_clear_list)}', *data_topic_clear_list[:5], sep='\n')
+# print(f'len data_title list: {len(data_title_clear_list)}', *data_title_clear_list[:5], sep='\n')
+
+# 1. Формирование обучающей выборки
+X_train, X_test, y_train, y_test = train_test_split(data_title_clear_list, data_topic_clear_list,
+                                                    train_size=0.67,
+                                                    random_state=42)
+
+print(f'len X_train: {len(X_train)}', *X_train[:5], sep='\n')
+print(f'len X_test: {len(X_test)}', *X_test[:5], sep='\n')
+print(f'len y_train: {len(y_train)}', *y_train[:5], sep='\n')
+print(f'len y_test: {len(y_test)}', *y_test[:5], sep='\n')
+
+
+# # 3. Векторизация документов: BOW
+# vectorizer_bow =
+# data_bow_vector =
 
